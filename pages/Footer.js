cec10134ef9e484b92c9f5363066065fcc106755
Lucas Pampolha <lpampolha@outlook.com>
2022-01-08 21:35:35 -0300

    Criação de Footer, Layout e Navbar - Aula 2

---
+++ pages/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer () {
+    return (
+        <footer>
+            <p>
+                <span>Pokenext</span> &copy; 2021
+            </p>
+        </footer>
+    )
+}
